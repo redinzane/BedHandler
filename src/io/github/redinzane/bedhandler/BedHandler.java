d83f39f2ccaadd8dc15c5add58f3ffbe70e3a54a
Moritz Schwab <moritz.schwab@ymail.com>
2014-01-31 23:27:32 +0100

    Finished basic functionality, Metrics
    - finished basic functionality (untested)
    -added Metrics

--- src/io/github/redinzane/bedhandler/BedHandler.java
+++ src/io/github/redinzane/bedhandler/BedHandler.java
@@ -1,8 +1,77 @@
 package io.github.redinzane.bedhandler;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BedHandler extends JavaPlugin
 {
-
+	// Configuration
+	private BedHandlerConfiguration config;
+	// Minecraft packet handling
+	private BedHandlerListener listener;
+	//ID of the runnable task
+	private int taskID = -1;
+	//Metrics
+	private Metrics metrics;
+	
+	@Override
+	public void onEnable()
+	{
+		//Creates a Config
+		config = new BedHandlerConfiguration(getConfig());
+				
+		if (!hasConfig()) 
+		{
+			getConfig().options().copyDefaults(true);
+			saveConfig();
+					
+			// Load it again
+			config = new BedHandlerConfiguration(getConfig());
+			getLogger().info("Creating default configuration.");
+		}
+		
+		listener = new BedHandlerListener();
+		getServer().getPluginManager().registerEvents(listener, this);
+		listener.bedDestroyer = config.getBeddestroyer();
+		listener.setDownWaitingTime = config.getSetdown();
+		
+		try
+		{
+			taskID = getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable(){public void run(){listener.updatePlayerList();} }, 0, 1);
+			if(taskID == -1)
+			{
+				throw new Exception("BedHandler Update Task could not be scheduled");
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		try 
+		{
+			metrics = new Metrics(this);
+			metrics.start();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Override
+    public void onDisable()
+    {
+		getServer().getScheduler().cancelTask(taskID);
+    }
+    
+	
+	//Checks if a config file exists
+    private boolean hasConfig() {
+		File file = new File(getDataFolder(), "config.yml");
+		return file.exists();
+	}
 }
--- src/io/github/redinzane/bedhandler/BedHandlerListener.java
+++ src/io/github/redinzane/bedhandler/BedHandlerListener.java
@@ -1,8 +1,86 @@
 package io.github.redinzane.bedhandler;
 
+import java.util.LinkedList;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class BedHandlerListener implements Listener
-{
-
+{	
+	boolean bedDestroyer = false;
+	int setDownWaitingTime = 1800000;
+	private long lastCall = 0;
+	private LinkedList<PlayerBed> playerList = new LinkedList<PlayerBed>();
+	private static class PlayerBed 
+	{
+		Player player;
+		Location location;
+		int countdown;
+		
+		PlayerBed(Player player, Location bed, int timeLeft)
+		{
+			this.player = player;
+			this.location = bed;
+			this.countdown = timeLeft;
+		}
+	}
+	
+	
+	
+	@EventHandler
+	public void onPlayerRespawn(PlayerRespawnEvent event)
+	{
+		Player playerRespawning = event.getPlayer();
+		if(event.isBedSpawn())
+		{
+			if(bedDestroyer)
+			{
+				Location location = playerRespawning.getBedSpawnLocation();
+				if(location != null)
+				{
+					location.getWorld().getBlockAt(location).breakNaturally();
+				}
+				playerRespawning.setBedSpawnLocation(null);
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerBedEnter(PlayerBedEnterEvent event)
+	{
+		Player playerEntering = event.getPlayer();
+		playerEntering.setBedSpawnLocation(null);
+		for(PlayerBed item: playerList)
+		{
+			if(item.player.equals(playerEntering))
+			{
+				playerList.remove(item);
+			}
+		}
+		playerList.add(new PlayerBed(playerEntering, event.getBed().getLocation(), setDownWaitingTime));
+		playerEntering.sendMessage(ChatColor.GRAY + "Your spawn will be set to this bed in " + setDownWaitingTime/60000 + " minutes... If it still exists then.");
+		event.setCancelled(true);
+	}
+	
+	public void updatePlayerList()
+	{
+		long currentTime = System.currentTimeMillis();
+		int deltaTime = (int) (currentTime-lastCall);
+		lastCall = currentTime;
+		for(PlayerBed item: playerList)
+		{
+			item.countdown -= deltaTime;
+			if(item.countdown <= 0)
+			{
+				item.player.setBedSpawnLocation(item.location);
+				item.player.sendMessage(ChatColor.GRAY + "Your spawn was set to your bed... If it still exists.");
+			}
+		}
+	}
+	
 }
